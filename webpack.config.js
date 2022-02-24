05e361754eeb9c94fb9a8d9d3c37e6bbec9a431e
Miha22 <deniel.alekseev0006@stud.hkr.se>
2022-02-24 01:10:40 +0100

    Added Webpack for sending scripts (required for a webpage to run something), to user browser.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,16 @@
 const path = require("path");
 //const config = require("./package.json");
-const file1 = 'sport-leader-script.js';
-const file2 = 'participate-script.js';
-const file0 = 'whitelist-client.js';
-const file4 = 'true-test-get-unique-id.js';
-//const output = 'fingerprint.js';
+const file1 = 'some-script.js';
 
 module.exports = {
     mode: 'development',
     entry: [
-        path.resolve(__dirname, './scripts/', file2),
+        path.resolve(__dirname, './scripts/', file1),
         //path.resolve(__dirname, './scripts/', file2)
     ],
     devtool: "source-map",
     output: {
         path: __dirname,
-        filename: `./public/bundle-${file2}`
+        filename: `./public/bundle-${file1}`
     }
 };
