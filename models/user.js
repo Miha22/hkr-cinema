f998ae4159c93d8af3697996aae8619667afc077
Mustafa <mustafa_9611@hotmail.com>
2022-03-03 16:00:32 +0100

    Added user schema and authentication file(in progress)

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const userschema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "Basic",
+        required: true,
+    },
+});
+
+const user = mongoose.model("user", userschema);
+module.exports = user;
