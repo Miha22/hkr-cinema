538921b0a48608f3b7ecefb8113366a8649c1eda
Mustafa <mustafa_9611@hotmail.com>
2022-03-05 15:40:04 +0100

    Added email to user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const userschema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
     username: {
         type: String,
         unique: true,
