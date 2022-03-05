f47371ad2ead5e21988f398593e6eb0cde4485af
Deniel Alekseev <daniel.neo.eu@icloud.com>
2022-03-05 12:53:31 +0100

    Added Babel, Webpack configurations for React lib.

---
+++ scripts/hello.js
@@ -0,0 +1,10 @@
+//import React from 'react';
+const React = require('react');
+const ReactDOM = require('react-dom');
+//import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Hello from react</h1>  
+};
+
+ReactDOM.render(<App/>, document.getElementById('hello'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 //const config = require("./package.json");
-const file1 = 'some-script.js';
+const file1 = 'hello.js';
 
 module.exports = {
     mode: 'development',
@@ -12,5 +13,24 @@ module.exports = {
     output: {
         path: __dirname,
         filename: `./public/bundle-${file1}`
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //       template: path.join(__dirname, "src", "index.html"),
+    //     }),
+    // ],
+    module: {
+        rules: [
+            {
+              test: /\.?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+              }
+            },
+          ]
     }
 };
